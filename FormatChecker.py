e2c27b962861e17bda5eeafe12c4d0d3639301a9
rajeswari <rajeswari284@gmail.com>
2013-04-11 18:41:52 -0400

    Changes made on Apr 11th

---
+++ FormatChecker.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import os
+import sys
+import re
+from collections import defaultdict
+import subprocess
+import stat
+
+def check(infile1, infile2, mode):
+    if mode == 'T':
+        print 'hello'
+        if os.stat(infile1).st_size == 0:
+            print 'You have submitted an empty t1 file.'
+            sys.exit(1)
+        elif re.match('\S+\.fasta$', infile1):
+            pattern = '>'
+            outfile = open('tmp.txt', 'w')
+            subprocess.call(['grep', pattern, infile1], stdout=outfile)
+            if not os.stat('tmp.txt').st_size > 0:
+                print 'Probably incorrect format for fasta file.'
+                sys.exit(1)
+        else:
+            print 'hello'
+            sys.exit(1)
+
+        if os.stat(infile2).st_size == 0:
+            print 'You have submitted an empty t2 file.'
+            sys.exit(1)
+        else:
+            infile2_handle = open(infile2, 'r')
+            firstline = infile2_handle.readline()
+            if not re.search('^\!gaf', firstline):
+                print "Error in input2 file format."
+                sys.exit(1)
+
+        
+    elif mode == 'F':
+        if os.stat(infile1).st_size == 0:
+            print 'You have submitted an empty t1 file.'
+            sys.exit(1)
+        else:
+            infile1_handle = open(infile1, 'r')
+            firstline = infile1_handle.readline()
+            if not re.search('^\!gaf', firstline):
+                print "Error in input1 file format"
+                sys.exit(1)
+
+        if os.stat(infile2).st_size == 0:
+            print 'You have submitted an empty t2 file.'
+            sys.exit(1)
+        else:
+            infile2_handle = open(infile2, 'r')
+            firstline = infile2_handle.readline()
+            if not re.search('^\!gaf', firstline):
+                print "Error in input2 file format"
+                sys.exit(1)
+        
+        
+
+
+if __name__ == '__main__':
+    infile1 = sys.argv[1]
+    infile2 = sys.argv[2]
+    mode = 'F'
+    check(infile1, infile2, mode)
